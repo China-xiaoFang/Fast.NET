0b787cad4e24dbe56d007c80f0c8e8bc76f0e618
小方 <2875616188@qq.com>
2024-01-29 17:54:41 +0800

    优化项目结构

--- frontend-template/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as FContextMenu } from "./FContextMenu";
-export { default as FDialog } from "./FDialog";
-export { default as FIcon } from "./FIcon";
-export { default as FImage } from "./FImage";
-export { default as FTable } from "./FTable";
