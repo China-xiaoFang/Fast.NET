9734f8ca37916976643f3f9ebac820e8cc7e7e28
小方 <2875616188@qq.com>
2024-01-29 17:30:31 +0800

    优化项目结构

--- frontend-template/src/components/FDialog/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Component } from "vue";
-
-declare const FDialog: Component;
-
-export default FDialog;
--- frontend-template/src/components/FIcon/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Component } from "vue";
-
-declare const FIcon: Component;
-
-export default FIcon;
