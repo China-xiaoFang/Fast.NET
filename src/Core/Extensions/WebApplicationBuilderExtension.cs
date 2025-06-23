02b754453d94e52736cea9a825a090937196067e
小方 <2875616188@qq.com>
2025-06-23 21:50:28 +0800

    修复了一些错误。

--- src/Core/Extensions/WebApplicationBuilderExtension.cs
+++ src/Core/Extensions/WebApplicationBuilderExtension.cs
@@ -60,7 +60,7 @@ public static class WebApplicationBuilderExtension
         var sb = new StringBuilder();
         sb.Append("\u001b[1m\u001b[34m");
         sb.Append(Environment.NewLine);
-        sb.Append($"    Fast.NET 程序启动时间：{DateTime.Now:yyyy-MM-dd HH:mm:ss}");
+        sb.Append($"Fast.NET 程序启动时间：{DateTime.Now:yyyy-MM-dd HH:mm:ss}");
         sb.Append(Environment.NewLine);
         sb.Append("\u001b[39m\u001b[22m\u001b[49m");
         sb.Append("\u001b[1m\u001b[32m");
@@ -71,15 +71,18 @@ public static class WebApplicationBuilderExtension
      |  __|   / _` | / __| | __|     | . ` | |  __|      | |   
      | |     | (_| | \__ \ | |_   _  | |\  | | |____     | |   
      |_|      \__,_| |___/  \__| (_) |_| \_| |______|    |_|   
-
 ");
         sb.Append("\u001b[39m\u001b[22m\u001b[49m");
         sb.Append("\u001b[1m\u001b[31m");
         sb.Append(Environment.NewLine);
+        sb.Append(Environment.NewLine);
         sb.Append("    Gitee：https://gitee.com/China-xiaoFang/Fast.NET");
         sb.Append(Environment.NewLine);
         sb.Append(Environment.NewLine);
+        sb.Append("\u001b[39m\u001b[22m\u001b[49m");
+        sb.Append("\u001b[1m\u001b[35m");
         sb.Append("    持续集百家所长，完善与丰富本框架基础设施，为.NET生态增加一种选择！");
+        sb.Append(Environment.NewLine);
         sb.Append(Environment.NewLine);
         sb.Append("期待您的PR，让.NET更好！");
         sb.Append(Environment.NewLine);
